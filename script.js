50ca28dffd3f3ffb8c84749c62b04ffca7a05c5e
Deltouille <alexandre.petit7676@gmail.com>
2021-09-06 13:19:45 +0200

    Modification html/js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let score;
-let scoreTotalJoueur1;
-let scoreTotalJoueur2;
-scoreTotalJoueur1 = 0;
-scoreTotalJoueur2 = 0;
+let scoreglobalJoueur1;
+let scoreglobalJoueur2;
+scoreglobalJoueur1 = 0;
+scoreglobalJoueur2 = 0;
 var dice;
 score = 0;
 
 //Initialisation des scores a 0 des deux joueurs
-document.getElementById('player1-total-score').innerText = '0';
-document.getElementById('player2-total-score').innerText = '0';
-document.getElementById('player1-current-score').innerText = '0';
-document.getElementById('player2-current-score').innerText = '0';
+document.getElementById('player1-global-score').innerText = '0';
+document.getElementById('player2-global-score').innerText = '0';
+document.getElementById('player1-round-score').innerText = '0';
+document.getElementById('player2-round-score').innerText = '0';
 //Cache les images des dès
 document.getElementById('dice1').style.display = "none";
 document.getElementById('dice2').style.display = "none";
@@ -22,10 +22,10 @@ document.getElementById('dice6').style.display = "none";
 //Réalisation de la fonction permettant de remettre a 0 la partie
 document.getElementById('btn-ng').addEventListener('click', function(){
     //Remise a 0 des scores
-    document.getElementById('player1-total-score').innerText = '0';
-    document.getElementById('player2-total-score').innerText = '0';
-    document.getElementById('player1-current-score').innerText = '0';
-    document.getElementById('player2-current-score').innerText = '0';
+    document.getElementById('player1-global-score').innerText = '0';
+    document.getElementById('player2-global-score').innerText = '0';
+    document.getElementById('player1-round-score').innerText = '0';
+    document.getElementById('player2-round-score').innerText = '0';
     //Cache les images des dès
     document.getElementById('dice1').style.display = "none";
     document.getElementById('dice2').style.display = "none";
@@ -100,17 +100,25 @@ document.getElementById('btn-roll').addEventListener('click', function(){
     if(document.getElementById('player1').classList.contains('current')){
         if(dice == 1){
             score = 0;
+            document.getElementById('player1-round-score').innerText = score;
             document.getElementById('player1').classList.toggle('current');
             document.getElementById('player2').classList.toggle('current');
+            document.getElementById('player1-name').classList.toggle('inactiveplayer');
+            document.getElementById('player2-name').classList.toggle('inactiveplayer');
+            
         }
-        document.getElementById('player1-current-score').innerText = score;
+        document.getElementById('player1-round-score').innerText = score;
     }else{
         if(dice == 1){
             score = 0;
+            document.getElementById('player2-round-score').innerText = score;
             document.getElementById('player1').classList.toggle('current');
             document.getElementById('player2').classList.toggle('current');
+            document.getElementById('player1-name').classList.toggle('inactiveplayer');
+            document.getElementById('player2-name').classList.toggle('inactiveplayer');
+            
         }
-        document.getElementById('player2-current-score').innerText = score;
+        document.getElementById('player2-round-score').innerText = score;
     }
     
 });
@@ -118,35 +126,41 @@ document.getElementById('btn-roll').addEventListener('click', function(){
 document.getElementById('btn-hold').addEventListener('click', function(){
     console.log(score);
     if(document.getElementById('player1').classList.contains('current')){
-        scoreTotalJoueur1 = scoreTotalJoueur1 + score;
+        scoreglobalJoueur1 = scoreglobalJoueur1 + score;
         score = 0;
-        document.getElementById('player1-total-score').innerText = scoreTotalJoueur1;
+        document.getElementById('player1-global-score').innerText = scoreglobalJoueur1;
         document.getElementById('player1').classList.toggle('current');
         document.getElementById('player2').classList.toggle('current');
+        document.getElementById('player1-name').classList.toggle('inactiveplayer');
+        document.getElementById('player2-name').classList.toggle('inactiveplayer');
     }else{
-        scoreTotalJoueur2 = scoreTotalJoueur2 + score;
+        scoreglobalJoueur2 = scoreglobalJoueur2 + score;
         score = 0;
-        document.getElementById('player2-total-score').innerText = scoreTotalJoueur2;
+        document.getElementById('player2-global-score').innerText = scoreglobalJoueur2;
         document.getElementById('player1').classList.toggle('current');
         document.getElementById('player2').classList.toggle('current');
+        document.getElementById('player1-name').classList.toggle('inactiveplayer');
+        document.getElementById('player2-name').classList.toggle('inactiveplayer');
     }
     
-    if(scoreTotalJoueur1 >= 100){
+    if(scoreglobalJoueur1 >= 100){
         alert('Le Joueur 1 a gagner la partie');
         score = 0;
-        scoreTotalJoueur1 = 0;
-        document.getElementById('player1-total-score').innerText = scoreTotalJoueur1;
+        scoreglobalJoueur1 = 0;
+        document.getElementById('player1-global-score').innerText = scoreglobalJoueur1;
+        document.getElementById('player1-round-score').innerText = score;
         document.getElementById('dice1').style.display = "none";
         document.getElementById('dice2').style.display = "none";
         document.getElementById('dice3').style.display = "none";
         document.getElementById('dice4').style.display = "none";
         document.getElementById('dice5').style.display = "none";
         document.getElementById('dice6').style.display = "none";
-    }else if(scoreTotalJoueur2 >= 100){
+    }else if(scoreglobalJoueur2 >= 100){
         alert('Le Joueur 2 a gagner la partie');
         score = 0;
-        scoreTotalJoueur2 = 0;
-        document.getElementById('player2-total-score').innerText = scoreTotalJoueur2;
+        scoreglobalJoueur2 = 0;
+        document.getElementById('player2-global-score').innerText = scoreglobalJoueur2;
+        document.getElementById('player2-round-score').innerText = score;
         document.getElementById('dice1').style.display = "none";
         document.getElementById('dice2').style.display = "none";
         document.getElementById('dice3').style.display = "none";
@@ -154,8 +168,8 @@ document.getElementById('btn-hold').addEventListener('click', function(){
         document.getElementById('dice5').style.display = "none";
         document.getElementById('dice6').style.display = "none";
     }
-    console.log('joueur 1 ' + scoreTotalJoueur1);
-    console.log('joueur 2 ' + scoreTotalJoueur2); 
+    console.log('joueur 1 ' + scoreglobalJoueur1);
+    console.log('joueur 2 ' + scoreglobalJoueur2); 
 });
 
 
